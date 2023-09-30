a8b467f2dd88c6470875b85af312809d50cbfb90
Shubham Vernekar <shubhamvernekar@192.168.1.2>
2023-09-30 20:47:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const userRoutes = require("./routes/UserRoute.js");
+
+app.use(bodyParser.json());
+
+
+app.use("/user", userRoutes);
+
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        message: "internal error"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("App listenging to port 3000");
+});
